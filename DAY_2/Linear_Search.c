7137f1c63d6454008d3934c5231be12ee2ce5721
Ayushi <133570484+Ayushi40804@users.noreply.github.com>
2024-07-26 08:10:42 +0530

    update

--- DAY_2/Linear_Search.c
+++ DAY_2/Linear_Search.c
@@ -1,24 +1,36 @@
 //Liner Search compares the element to be searched with each element of the array one by one.
 //No of elements is equal to the no of steps required to solve the problem.
 #include <stdio.h>
-int main()
-{
-    int arr[10], i,n,x;
-    printf("Enter the numbers of elements in array: \n");
-    scanf("%d",&n);
+void array(int n);
+void linear_search(int arr[], int n);
+void array(int n){
+    int i, arr[n];
     printf("Enter the elements of array: \n");
     for(i=0 ; i<n ; i++){
         scanf("%d" , &arr[i]);
     }
+    linear_search(arr,n);
+}
+
+void linear_search(int arr[], int n){
+    int i,x;
     printf("LINEAR SEARCH\n");
     printf("Enter the element to be searched in the array: \n");
     scanf("%d",&x);
-    printf("TEST");
+    //printf("TEST");
     for (i=0;i<n;i++){
         if (arr[i]==x){
             printf("The element is found at the %d position in the array.",i+1);
-            return 0;
+            return;
         }
     }
-    return 1;
+    return;
+}
+    
+int main()
+{
+    int n;
+    printf("Enter the numbers of elements in array: \n");
+    scanf("%d",&n);
+    array(n);
 }
