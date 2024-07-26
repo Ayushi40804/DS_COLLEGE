0a4853d07146144fa5c336382ff8109981bd335a
Ayushi <133570484+Ayushi40804@users.noreply.github.com>
2024-07-26 08:23:45 +0530

    Update

--- DAY_1/2.c
+++ DAY_1/2.c
@@ -6,11 +6,12 @@ int main()
     char p[20]; 
 char *s = "string"; 
 int length = strlen(s); 
-printf("%d\n",length);
+//printf("%d\n",length);
 int i; 
 for (i = 0; i < length; i++) 
-     p[i] = s[length - i]; //This program ends at the null string character.
+     p[i] = s[length - i]; //This program access s[6] which is the null string character.
     //p[length] = '\0'; //adding null character at the end of the string
+    // printf("TEST\n");
 printf("%s",p); 
     return 0;
 }
--- DAY_2/Binary_Search.c
+++ DAY_2/Binary_Search.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+void array(int n);
+void binary_search(int arr[], int n);
+
+void array(int n){
+    int i, arr[n];
+    printf("Enter the elements of array: \n");
+    for(i=0 ; i<n ; i++){
+        scanf("%d" , &arr[i]);
+    }
+    binary_search(arr,n);
+}
+
+void binary_search(int arr,int n,int x){
+    
+    if (x<arr[n/2]){
+        binary_search(arr[n/2], int n/2)    }
+    if(x>arr[n/2]){
+
+    }
+    if(x==arr[n/2]){
+
+    }
+    else{
+        printf("ERROR!");
+    }
+    return;
+
+}
 int main()
 {
     
