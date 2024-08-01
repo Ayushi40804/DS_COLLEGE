15e4695f323b96e93cbb02a8c89df21ba329f54d
Ayushi <133570484+Ayushi40804@users.noreply.github.com>
2024-08-01 10:30:44 +0530

    UPDATE

---
+++ DAY_2_THEORY/Complex_Number.c
@@ -0,0 +1,38 @@
+//read two complex numbers and create two functions to add and multiply them replace void return type by complex 
+#include <stdio.h>
+typedef struct complex
+{
+    float real;
+    float imag;
+} complex;
+complex add(complex n1, complex n2);
+complex multiply(complex n1, complex n2);
+int main()
+{
+    complex n1, n2, result_add, result_multiply;
+    printf("For first complex number \n");
+    printf("Enter the real and imaginary parts respectively: ");
+    scanf("%f %f", &n1.real, &n1.imag);
+    printf("For second complex number \n");
+    printf("Enter the real and imaginary parts respectively: ");
+    scanf("%f %f", &n2.real, &n2.imag);
+    result_add = add(n1, n2);
+    result_multiply = multiply(n1, n2);
+    printf("Sum = %.1f + %.1fi\n", result_add.real, result_add.imag);
+    printf("Multiplication = %.1f + %.1fi\n", result_multiply.real, result_multiply.imag);
+    return 0;
+}
+complex add(complex n1, complex n2)
+{
+    complex temp;
+    temp.real = n1.real + n2.real;
+    temp.imag = n1.imag + n2.imag;
+    return temp;
+}
+complex multiply(complex n1, complex n2)
+{
+    complex temp;
+    temp.real = n1.real * n2.real - n1.imag * n2.imag;
+    temp.imag = n1.real * n2.imag + n1.imag * n2.real;
+    return temp;
+}
---
+++ DAY_2_THEORY/Complex_Numbers_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+typedef struct complex_struct
+{
+    float real;
+    float imag;
+} complex;
+int main()
+{
+    complex n[10];
+    for (int i = 0; i < 10; i++)
+    {
+        printf("For %d complex number", i + 1);
+        scanf("%f %f", &n[i].real, &n[i].imag);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Complex number %d = %.1f + %.1fi\n", i + 1, n[i].real, n[i].imag);
+    }
+    return 0;
+}
